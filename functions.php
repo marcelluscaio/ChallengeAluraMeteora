0f2e1b3cb5052e09eecb85d59e593a5fed59723a
Caio Cabral <ocaiocabral@gmail.com>
2023-06-21 22:03:35 -0300

    feat: header and footer php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/* function meteora_register_style() {
+   wp_register_style('general-style', get_template_directory_uri().'/style.css');
+}
+
+
+
+function meteora_enqueue_style(){
+   wp_enqueue_style('general-style');
+}
+
+add_action('init', 'meteora_register_style'); 
+add_action('wp_enqueue_scripts', ' meteora_enqueue_style'); */
+
+function meteora_enqueue_style(){
+   wp_enqueue_style('general-style', get_template_directory_uri().'/style.css');
+}
+
+add_action('wp_enqueue_scripts', ' meteora_enqueue_style');
+?> 
--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<?php
-wp_enqueue_style('style.css')
+<?php 
+get_header();
 
+
+get_footer();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Meteora</title>
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
-   <link rel="stylesheet" href="style.css">
-</head>
-<body>
-   <header></header>
-</body>
-</html>
+
+
