2228936a39ba9fd81c1d57aa4a788232edc0800d
Caio Cabral <ocaiocabral@gmail.com>
2023-06-24 11:50:36 -0300

    feat: more of the sites html

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 
 <header class="header">
    <nav>
-      <ul>
+      <ul class="container">
          <li>logo</li>
          <li>hamburguer</li>
          <li>Home</li>
@@ -26,7 +26,65 @@
                <button>Buscar</button>
             </form>
          </li>
-
       </ul>
    </nav>
 </header>
+
+<!-- CARROSSEL -->
+
+<section>
+   <img src="" alt=""/>
+</section>
+
+
+
+
+<!-- BUSCA CATEGORIA -->
+
+<section class="container">
+   <h2 class="title">Busque por categoria:</h2>
+   <!-- Fazer um filtro por categora do WordPress -->
+   <!-- Puxar automaticamente por categorias -->
+   <ul>
+      <li>
+         <a href="">
+            <img src="" alt=""/>
+            <h3 class="title title--extra-small">Camisetas</h3>
+         </a>
+      </li>
+      <li>
+         <a href="">
+            <img src="" alt=""/>
+            <h3 class="title title--extra-small">Bolsas</h3>
+         </a>
+      </li>
+      <li>
+         <a href="">
+            <img src="" alt=""/>
+            <h3 class="title title--extra-small">Calçados</h3>
+         </a>
+      </li>
+      <li>
+         <a href="">
+            <img src="" alt=""/>
+            <h3 class="title title--extra-small">Calças</h3>
+         </a>
+      </li>
+      <li>
+         <a href="">
+            <img src="" alt=""/>
+            <h3 class="title title--extra-small">Casacos</h3>
+         </a>
+      </li>
+      <li>
+         <a class="" href="">
+            <img src="" alt=""/>
+            <h3 class="title title--extra-small">Óculos</h3>
+         </a>
+      </li>
+   </ul>
+</section>
+
+
+<!-- DESTAQUES -->
+
