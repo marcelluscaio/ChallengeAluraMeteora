f99407be90cbcb38551c8380ea6083999dd793fb
Caio Cabral <ocaiocabral@gmail.com>
2023-07-06 17:36:46 -0300

    feat: setting carousel content on backend

--- Template_parts/Home/01_carousel.php
+++ Template_parts/Home/01_carousel.php
@@ -1,9 +1,16 @@
 <?php
-
+   $slides = get_field('carousel-slides');
 ?>
 
-<!-- CARROSSEL -->
-
 <section>
-   <img src="" alt=""/>
+
+<?php
+foreach($slides as $slide):
+?>
+   <img src="<?= $slide["carousel_image"] ?>" alt=""/>
+   <p><?= $slide["carousel_title"] ?></p>
+   <p><?= $slide["carousel_title_2"] ?></p>
+<?php 
+endforeach;
+?>
 </section>
