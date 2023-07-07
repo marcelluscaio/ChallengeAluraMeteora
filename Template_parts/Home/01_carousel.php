e765265088ac74eccf353745c8a92d99d5372309
Caio Cabral <ocaiocabral@gmail.com>
2023-07-07 18:06:10 -0300

    feat: conditional rendering for img and p on carousel slide

--- Template_parts/Home/01_carousel.php
+++ Template_parts/Home/01_carousel.php
@@ -1,16 +1,21 @@
 <?php
-   $slides = get_field('carousel-slides');
+	$slides = get_field('carousel-slides');
 ?>
 
 <section>
-
 <?php
 foreach($slides as $slide):
+	if($slide["carousel_image"]):
 ?>
-   <img src="<?= $slide["carousel_image"] ?>" alt=""/>
-   <p><?= $slide["carousel_title"] ?></p>
-   <p><?= $slide["carousel_title_2"] ?></p>
-<?php 
+	<img src="<?= $slide["carousel_image"] ?>" alt=""/>
+<?php if($slide["carousel_title"]): ?>
+	<p><?= $slide["carousel_title"] ?></p>
+<?php endif ?>
+<?php if($slide["carousel_title_2"]): ?>
+	<p><?= $slide["carousel_title_2"] ?></p>
+<?php endif ?>
+<?php
+   endif;
 endforeach;
 ?>
 </section>
