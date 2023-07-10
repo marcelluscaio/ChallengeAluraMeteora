70dcf960ddad885845b2435b2d07b8bdd04cf680
Caio Cabral <ocaiocabral@gmail.com>
2023-07-10 19:15:42 -0300

    feat: carousel controls

--- Template_parts/Home/01_carousel.php
+++ Template_parts/Home/01_carousel.php
@@ -7,7 +7,7 @@
 $index = 0;
 foreach($slides as $slide):
 	$index++;
-	$slideClassName = '.carousel__slide--'.$index;
+	$slideClassName = '.carousel__track__slide--'.$index;
 	$hasBg = strlen($slide["carousel_bg"]) > 0;
 ?>
 <?= $slideClassName ?>{
@@ -28,25 +28,44 @@ endforeach;
 ?>
 </style>
 
+
 <section class="carousel" aria-label="Carousel">
 
+	<div class="carousel__controls">
+		<div class="carousel__controls__navigation">
+<?php 
+$index = 0;
+foreach($slides as $slide):	
+	$index++;
+?>
+			<button class="navigation--<?= $index ?> "></button>
+<?php endforeach ?>
+		</div>
+		<div class="carousel__controls__arrows">
+			<button class="carousel__controls__arrows__arrow"></button>
+			<button class="carousel__controls__arrows__arrow"></button>
+		</div>
+	</div>
+
+	<div class="carousel__track">
 <?php
 $index = 0;
 foreach($slides as $slide):
 	$index++;
 	if($slide["carousel_image"]):
-		$bgClass = ($slide["carousel_title"] || $slide["carousel_title_2"]) && $slide["carousel_bg"] ? "carousel__slide--"."bg" : "";
+		$bgClass = ($slide["carousel_title"] || $slide["carousel_title_2"]) && $slide["carousel_bg"] ? "carousel__track__slide--"."bg" : "";
 ?>
-	<div class="carousel__slide carousel__slide--<?= $index ?> <?= $bgClass ?>" role="group" aria-roledescription="Slide">
+		<div class="carousel__track__slide carousel__track__slide--<?= $index ?> <?= $bgClass ?>" role="group" aria-roledescription="Slide">
 <?php if($slide["carousel_title"]): ?>
-		<p class="title "><?= $slide["carousel_title"] ?></p>
+			<p class="title "><?= $slide["carousel_title"] ?></p>
 <?php endif ?>
 <?php if($slide["carousel_title_2"]): ?>
-		<p class="regular-text"><?= $slide["carousel_title_2"] ?></p>
+			<p class="regular-text"><?= $slide["carousel_title_2"] ?></p>
 <?php endif ?>
-	</div>
+		</div>
 <?php
    endif;
 endforeach;
 ?>
+	</div>
 </section>
