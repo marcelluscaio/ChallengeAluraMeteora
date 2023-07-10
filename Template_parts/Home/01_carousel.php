b6392d5efe04abf6e9fe002addd27cea6d05eee2
Caio Cabral <ocaiocabral@gmail.com>
2023-07-10 15:51:57 -0300

    feat: background to images

--- Template_parts/Home/01_carousel.php
+++ Template_parts/Home/01_carousel.php
@@ -7,26 +7,39 @@
 $index = 0;
 foreach($slides as $slide):
 	$index++;
-	$className = '.carousel__slide--'.$index;
+	$slideClassName = '.carousel__slide--'.$index;
+	$hasBg = strlen($slide["carousel_bg"]) > 0;
 ?>
-<?= $className ?>{
+<?= $slideClassName ?>{
 	background-image: url('<?=  $slide["carousel_image"] ?>');
 }
+
+<?php 
+	if($hasBg):
+?>
+
+<?= $slideClassName ?>::after{
+	background-color: <?=  $slide["carousel_bg"] ?>;
+}
+
 <?php
+	endif;
 endforeach;
 ?>
 </style>
 
 <section class="carousel" aria-label="Carousel">
+
 <?php
 $index = 0;
 foreach($slides as $slide):
 	$index++;
 	if($slide["carousel_image"]):
+		$bgClass = ($slide["carousel_title"] || $slide["carousel_title_2"]) && $slide["carousel_bg"] ? "carousel__slide--"."bg" : "";
 ?>
-	<div class="carousel__slide carousel__slide--<?= $index ?>" role="group" aria-roledescription="Slide">
+	<div class="carousel__slide carousel__slide--<?= $index ?> <?= $bgClass ?>" role="group" aria-roledescription="Slide">
 <?php if($slide["carousel_title"]): ?>
-		<p class="title"><?= $slide["carousel_title"] ?></p>
+		<p class="title "><?= $slide["carousel_title"] ?></p>
 <?php endif ?>
 <?php if($slide["carousel_title_2"]): ?>
 		<p class="regular-text"><?= $slide["carousel_title_2"] ?></p>
