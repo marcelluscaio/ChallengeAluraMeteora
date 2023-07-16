044fc8d8d02d786124d7af07cc992d2e7092145e
Caio Cabral <ocaiocabral@gmail.com>
2023-07-15 22:59:18 -0300

    feat: creating CPT

--- functions.php
+++ functions.php
@@ -60,38 +60,38 @@ remove_action('wp_head', '_admin_bar_bump_cb');
 
 
 function cm_add_style(){
-   wp_register_style('cm_reset', get_template_directory_uri().'/dist/css/general/style-guide/reset.css');
-   wp_enqueue_style('cm_reset');
+	wp_register_style('cm_reset', get_template_directory_uri().'/dist/css/general/style-guide/reset.css');
+	wp_enqueue_style('cm_reset');
 
-   wp_register_style('cm_layout', get_template_directory_uri().'/dist/css/general/style-guide/layout.css');
-   wp_enqueue_style('cm_layout');
+	wp_register_style('cm_layout', get_template_directory_uri().'/dist/css/general/style-guide/layout.css');
+	wp_enqueue_style('cm_layout');
 
-   wp_register_style('cm_fonts', get_template_directory_uri().'/dist/css/general/style-guide/fonts.css');
-   wp_enqueue_style('cm_fonts');
+	wp_register_style('cm_fonts', get_template_directory_uri().'/dist/css/general/style-guide/fonts.css');
+	wp_enqueue_style('cm_fonts');
 
-   wp_register_style('cm_colors', get_template_directory_uri().'/dist/css/general/style-guide/colors.css');
-   wp_enqueue_style('cm_colors');
+	wp_register_style('cm_colors', get_template_directory_uri().'/dist/css/general/style-guide/colors.css');
+	wp_enqueue_style('cm_colors');
 
-   wp_register_style('cm_buttons', get_template_directory_uri().'/dist/css/general/components/buttons.css');
-   wp_enqueue_style('cm_buttons');
+	wp_register_style('cm_buttons', get_template_directory_uri().'/dist/css/general/components/buttons.css');
+	wp_enqueue_style('cm_buttons');
 
-   wp_register_style('cm_header', get_template_directory_uri().'/dist/css/general/header/header.css');
-   wp_enqueue_style('cm_header');
+	wp_register_style('cm_header', get_template_directory_uri().'/dist/css/general/header/header.css');
+	wp_enqueue_style('cm_header');
 
-   wp_register_style('cm_footer', get_template_directory_uri().'/dist/css/general/footer/footer.css');
-   wp_enqueue_style('cm_footer');
+	wp_register_style('cm_footer', get_template_directory_uri().'/dist/css/general/footer/footer.css');
+	wp_enqueue_style('cm_footer');
 
-   wp_register_style('cm_carousel', get_template_directory_uri().'/dist/css/home/carousel.css');
-   wp_enqueue_style('cm_carousel');
+	wp_register_style('cm_carousel', get_template_directory_uri().'/dist/css/home/carousel.css');
+	wp_enqueue_style('cm_carousel');
 }
 add_action('wp_enqueue_scripts', 'cm_add_style');
 
 function cm_add_script(){
-    wp_register_script('cm_hamburger_menu', get_template_directory_uri().'/dist/js/general/hamburger-menu.js');
-    wp_enqueue_script('cm_hamburger_menu', '', array(), '', true);
+	wp_register_script('cm_hamburger_menu', get_template_directory_uri().'/dist/js/general/hamburger-menu.js');
+	wp_enqueue_script('cm_hamburger_menu', '', array(), '', true);
 
-    wp_register_script('cm_carousel', get_template_directory_uri().'/dist/js/home/carousel.js');
-    wp_enqueue_script('cm_carousel', '', array(), '', true);
+	wp_register_script('cm_carousel', get_template_directory_uri().'/dist/js/home/carousel.js');
+	wp_enqueue_script('cm_carousel', '', array(), '', true);
 }
 add_action('wp_enqueue_scripts', 'cm_add_script');
 
@@ -105,8 +105,24 @@ add_theme_support('menus');
 register_nav_menu( 'primary', 'Menu home' ); /*Nome do menu no PHP, nome do menu no painel administrativo*/
 
 
+//Criar custom post type
+function cm_create_cpt_products(){
+	register_post_type('cm_produtos',
+		array(
+			'labels' => array(
+				'name' => __('Produtos'),
+				'singular_name' => __('Produto')
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'produtos'),
+			'show_in_rest' => true,
+			'menu_icon' => 'dashicons-cart'
+		)
+	);
+}
 
-
+add_action( 'init', 'cm_create_cpt_products' );
 
 
 
