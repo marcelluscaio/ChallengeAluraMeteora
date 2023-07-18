bae982de371234aeb2274d8c8b446e5e771dc66c
Caio Cabral <ocaiocabral@gmail.com>
2023-07-18 00:47:14 -0300

    feat: footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
    <div class="container">
-      <p>2023 © Desenvolvido por Caio Cabral para o sétimo Challenge Front-End da Alura | Projeto fictício sem fins comerciais</p>
+      <p class="regular-text regular-text--small">2023 © Desenvolvido por Caio Cabral para o sétimo Challenge Front-End da Alura | Projeto fictício sem fins comerciais</p>
    </div>
 </footer>
 
