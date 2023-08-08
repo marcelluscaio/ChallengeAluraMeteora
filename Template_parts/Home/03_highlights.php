ce0ecbb628750d1246eed074c3008d85d8ae8eec
Caio Cabral <ocaiocabral@gmail.com>
2023-08-08 12:02:01 -0300

    feat: modal mobile version finished

--- Template_parts/Home/03_highlights.php
+++ Template_parts/Home/03_highlights.php
@@ -53,7 +53,7 @@ foreach($todas_as_cores as $cor) :
 $classe = '.input-'.strtolower(str_replace(' ', '-', $cor->name));
 $hexa_cor = get_field('cor','cm_colors'.'_'.$cor->term_id);
 ?>
-<?= $classe ?>:checked{
+.modal__form__color-size <?= $classe ?>:checked{
 	background-color: <?= $hexa_cor ?>;
 }
 
@@ -92,11 +92,12 @@ $counter++;
 				</header>
 				<div>
 					<img src="<?= $post['imagem'] ?>" alt="<?= $post['nome'] ?>" />
-					<h3 class="title title--extra-small"><?= $post['nome'] ?></h3>
-					<p class="description regular-text regular-text--small "><?= $post['descricao'] ?></p>
-					<p class="price title title--extra-small ">R$ <?= $post['preco'] ?></p>
-					<form>
-						<h4>Cores</h4>
+					<div>
+						<h3 class="title title--extra-small"><?= $post['nome'] ?></h3>
+						<p class="description regular-text regular-text--small "><?= $post['descricao'] ?></p>
+						<p class="price title title--extra-small ">R$ <?= $post['preco'] ?></p>
+						<form>
+							<h4 class="title title--extra-small regular-text--small">Cores</h4>
 						
 <?php 
 foreach($post['cores'] as $cor):
@@ -104,27 +105,28 @@ foreach($post['cores'] as $cor):
 	$input_id = "input-".$cor."-".strtolower(str_replace(' ', '-', $post['nome']));
 	$input_class = 'input-'.strtolower(str_replace(' ', '-', $cor));
 ?>
-						<div class="modal__form__color-size">
-							<label for="<?= $input_id ?>" class="<?= $input_class ?> regular-text regular-text--small"><?= $cor ?></label>
-							<input type="radio" value="<?= $cor ?>" id="<?= $input_id ?>" class="<?= $input_class ?>" name="<?= $input_name ?>" />
-						</div>
+							<div class="modal__form__color-size">
+								<label for="<?= $input_id ?>" class="<?= $input_class ?> regular-text regular-text--small"><?= $cor ?></label>
+								<input type="radio" value="<?= $cor ?>" id="<?= $input_id ?>" class="<?= $input_class ?>" name="<?= $input_name ?>" />
+							</div>
 <?php 
 endforeach;
 ?>
-						<h4>Tamanho:</h4>
+							<h4 class="title title--extra-small regular-text--small">Tamanho:</h4>
 <?php 
 foreach($post['sizes'] as $size):
 $input_name = "input-size-".strtolower(str_replace(' ', '-', $post['nome']));
 ?>
-						<div class="modal__form__color-size">
-							<label><?= $size ?></label>
-							<input type="radio" value="<?= $size ?>" name="<?= $input_name ?>" />
-						</div>
+							<div class="modal__form__color-size">
+								<label class="regular-text regular-text--small"><?= $size ?></label>
+								<input type="radio" value="<?= $size ?>" name="<?= $input_name ?>" />
+							</div>
 <?php 
 endforeach
 ?>
-						<button>Adicionar à sacola</button>
-					</form>
+							<button class="button">Adicionar à sacola</button>
+						</form>
+					</div>
 				</div>
 			</dialog>
 		</li>
