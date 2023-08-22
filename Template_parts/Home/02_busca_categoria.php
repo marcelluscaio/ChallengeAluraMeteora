30bbbfb595fe9f566be37828795bfac2f65800d0
Caio Cabral <ocaiocabral@gmail.com>
2023-08-22 15:31:21 -0400

    fix: general refactor, and passing dynamic url on categories

--- Template_parts/Home/02_busca_categoria.php
+++ Template_parts/Home/02_busca_categoria.php
@@ -1,25 +1,7 @@
 <?php
-//QUando clicar em uma categoria, quero mostrar todos os posts daquela categoria
-//fazer requisicao mostrando somente aqueles produtos
-/* $query = new WP_Query(
-	array('post_type'=>'cm_produtos' ,
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'cm_types',
-						'field' => 'slug',
-						'terms' => 'Camisa',
-					)
-				) 
-	)
-);
+$website_url = get_site_url();
 
-while($query->have_posts()){
-	$query->the_post();
-	$post = $query->post;
-	$title = $query->post->post_title;
-} */
-
-//Mostra todos os tipos de produto
+//Get all product types
 $produtos = get_terms('cm_types');
 $array_produtos = array();
 foreach($produtos as $produto){
@@ -31,14 +13,12 @@ foreach($produtos as $produto){
 
 <section class="search container">
 	<h2 class="title">Busque por categoria:</h2>
-	<!-- Fazer um filtro por categora do WordPress -->
-	<!-- Puxar automaticamente por categorias (Categoria -> categoria) -->
 	<ul class="search__items">
 <?php
 foreach($array_produtos as $produto):
 ?>
 		<li>
-				<a href="http://alurachallengemeteora.local/todos-produtos/?produto=<?= $produto['nome'] ?>">
+				<a href="<?= $website_url ?>/todos-produtos/?produto=<?= $produto['nome'] ?>">
 					<img src="<?= $produto['url'] ?>" alt="<?= $produto['nome'] ?>"/>
 					<h3 class="title title--extra-small"><?= $produto['nome'] ?></h3>
 				</a>
@@ -46,42 +26,6 @@ foreach($array_produtos as $produto):
 <?php
 endforeach
 ?>
-		<!-- <li>
-				<a href="">
-					<img src="" alt=""/>
-					<h3 class="title title--extra-small">Camisetas</h3>
-				</a>
-		</li>
-		<li>
-				<a href="">
-					<img src="" alt=""/>
-					<h3 class="title title--extra-small">Bolsas</h3>
-				</a>
-		</li>
-		<li>
-				<a href="">
-					<img src="" alt=""/>
-					<h3 class="title title--extra-small">Calçados</h3>
-				</a>
-		</li>
-		<li>
-				<a href="">
-					<img src="" alt=""/>
-					<h3 class="title title--extra-small">Calças</h3>
-				</a>
-		</li>
-		<li>
-				<a href="">
-					<img src="" alt=""/>
-					<h3 class="title title--extra-small">Casacos</h3>
-				</a>
-		</li>
-		<li>
-				<a class="" href="">
-					<img src="" alt=""/>
-					<h3 class="title title--extra-small">Óculos</h3>
-				</a>
-		</li> -->
 	</ul>
 </section>
 
--- Template_parts/Home/03_highlights.php
+++ Template_parts/Home/03_highlights.php
@@ -7,30 +7,28 @@ $posts = get_posts(array(
     'meta_value'    => true
 ));
 
-$posts_information = array();
-
 //getting information from posts
+$posts_information = array();
 foreach($posts as $post){
 	$nome = get_field('nome_produto');
 	$descricao = get_field('descricao_produto');
 	$imagem = get_field('imagem_produto');
 	$preco = get_field('preco_produto');
-
 	$preco = number_format(floatval($preco), 2, ',', '.');
 
-//getting product's color
+//getting product's color - Custom taxonomy
 	$cores_object = get_the_terms($post, 'cm_colors');
 	$cores = array();
 	foreach($cores_object as $cor){
 		array_push($cores, $cor->name);
-	}
+	};
 
-	//getting product's size
+	//getting product's size - Custom taxonomy
 	$sizes_object = get_the_terms($post, 'cm_sizes');
 	$sizes = array();
 	foreach($sizes_object as $size){
 		array_push($sizes, $size->name);
-	}
+	};
 
 	array_push($posts_information, array(
 		'nome' => $nome,
@@ -53,6 +51,7 @@ foreach($todas_as_cores as $cor) :
 $classe = '.input-'.strtolower(str_replace(' ', '-', $cor->name));
 $hexa_cor = get_field('cor','cm_colors'.'_'.$cor->term_id);
 ?>
+
 .modal__form__color-size <?= $classe ?>:checked{
 	background-color: <?= $hexa_cor ?>;
 }
@@ -85,73 +84,24 @@ $counter++;
 					<button class="button regular-text open-modal modal--<?= $counter ?>">Ver mais</button>
 				</div>
 			</article>
-			<?php get_template_part('Template_parts/Modal/modal', '', 
-				array(
-					'counter' => $counter,
-					'imagem' => $post['imagem'],
-					'nome' => $post['nome'],
-					'descricao' => $post['descricao'],
-					'preco' => $post['preco'],
-					'cores' => $post['cores'],
-					'sizes' => $post['sizes'],
-				)
-			) ?>
+<?php 
+get_template_part('Template_parts/Modal/modal', '', 
+	array(
+		'counter' => $counter,
+		'imagem' => $post['imagem'],
+		'nome' => $post['nome'],
+		'descricao' => $post['descricao'],
+		'preco' => $post['preco'],
+		'cores' => $post['cores'],
+		'sizes' => $post['sizes'],
+	)
+) 
+?>
 
-
-
-			<?php 
-				if(1>2):
-			?>
-			<dialog class="modal modal--<?= $counter ?>">
-				<header>
-					<h2 class="regular-text">Confira detalhes sobre o produto</h2>
-					<button class="button button--white close-modal modal--<?= $counter ?>">X</button>
-				</header>
-				<div>
-					<img src="<?= $post['imagem'] ?>" alt="<?= $post['nome'] ?>" />
-					<div>
-						<h3 class="title title--extra-small"><?= $post['nome'] ?></h3>
-						<p class="description regular-text regular-text--small "><?= $post['descricao'] ?></p>
-						<p class="price title title--extra-small ">R$ <?= $post['preco'] ?></p>
-						<form>
-							<h4 class="title title--extra-small regular-text--small">Cores</h4>
-						
-<?php 
-foreach($post['cores'] as $cor):
-	$input_name = "input-".strtolower(str_replace(' ', '-', $post['nome']));
-	$input_id = "input-".$cor."-".strtolower(str_replace(' ', '-', $post['nome']));
-	$input_class = 'input-'.strtolower(str_replace(' ', '-', $cor));
-?>
-							<div class="modal__form__color-size">
-								<label for="<?= $input_id ?>" class="<?= $input_class ?> regular-text regular-text--small"><?= $cor ?></label>
-								<input type="radio" value="<?= $cor ?>" id="<?= $input_id ?>" class="<?= $input_class ?>" name="<?= $input_name ?>" />
-							</div>
-<?php 
-endforeach;
-?>
-							<h4 class="title title--extra-small regular-text--small">Tamanho:</h4>
-<?php 
-foreach($post['sizes'] as $size):
-$input_name = "input-size-".strtolower(str_replace(' ', '-', $post['nome']));
-?>
-							<div class="modal__form__color-size">
-								<label class="regular-text regular-text--small"><?= $size ?></label>
-								<input type="radio" value="<?= $size ?>" name="<?= $input_name ?>" />
-							</div>
-<?php 
-endforeach
-?>
-							<button class="button">Adicionar à sacola</button>
-						</form>
-					</div>
-				</div>
-			</dialog>
-		<?php
-			endif
-		?>
 		</li>
+
 <?php
 endforeach
 ?>
-   </ul>
+  </ul>
 </section>
