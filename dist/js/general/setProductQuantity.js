52c8ffeff475775fb06dc6f4b9297888b3a491aa
Caio Cabral <ocaiocabral@gmail.com>
2023-08-30 15:43:04 -0400

    feat: getting input from the div

--- dist/js/general/setProductQuantity.js
+++ dist/js/general/setProductQuantity.js
@@ -1,3 +1,12 @@
-//pegar os inputs
-// colocar event listener
+const fieldsColorSize = document.querySelectorAll(".modal__form__color-size");
+
+fieldsColorSize.forEach(field => 
+  field.addEventListener("click", e => {
+    if(e.target.matches("input")) console.log(e.target)
+  }
+))
+
+// colocar event listener nas divs. Checar o que t[a embaixo
+// 
 // usar https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+//https://gomakethings.com/a-native-vanilla-javascript-way-to-get-the-closest-matching-parent-element/
