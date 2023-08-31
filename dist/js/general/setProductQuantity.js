a3102ed15e3dee3f8a5687232a7b3b316dbb2c73
Caio Cabral <ocaiocabral@gmail.com>
2023-08-31 18:56:58 -0400

    feat: getting available field when selecting complimentary field (color -> size, size -> color

--- dist/js/general/setProductQuantity.js
+++ dist/js/general/setProductQuantity.js
@@ -7,11 +7,21 @@ fieldsColorSize.forEach(field =>
       const type = e.target.dataset.type;
       const value = e.target.value;
       select.dataset[type] = value;
+      
+      const dialog = select.closest('dialog');
+      const title = dialog.querySelector('h3').innerText;
+      const thisProduct = productInformation.find(item => item[0] === title);
+      const availableSizesOrColors = thisProduct[1].filter(item => item[type] === select.dataset[type]).map(item => {
+        const {quantidade, ...remaining} = item;
+        const target = Object.keys(remaining).find(key => key !== type);
+        return item[target]
+      }
+      );
+
+      //disabilitar os outros campos (se cor -> tamanho, se tamanho -> cor)
+      console.log(availableSizesOrColors);
 
       if(select.dataset.cor && select.dataset.tamanho){
-        const dialog = select.closest('dialog');
-        const title = dialog.querySelector('h3').innerText;
-        const thisProduct = productInformation.find(item => item[0] === title);
         const quantity = thisProduct[1].find(item => item.cor === select.dataset.cor && item.tamanho === select.dataset.tamanho).quantidade;
         
         while(select.firstChild){
@@ -25,16 +35,14 @@ fieldsColorSize.forEach(field =>
           select.appendChild(option)
         }
       }
+
     }
   }
 ))
 
-// colocar event listener nas divs. Checar o que t[a embaixo
-// 
-// usar https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
-//https://gomakethings.com/a-native-vanilla-javascript-way-to-get-the-closest-matching-parent-element/
-
+//Refs
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+// https://gomakethings.com/a-native-vanilla-javascript-way-to-get-the-closest-matching-parent-element/
 //https://gomakethings.com/using-multiple-selectors-with-queryselector-queryselectorall-closest-and-matches/
 //https://gomakethings.com/event-delegation-and-nested-elements/
-
 //https://css-tricks.com/practical-use-cases-for-javascripts-closest-method/
